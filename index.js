ed13ed07583017d7748e9764c80135b235706a03
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2023-10-19 09:14:40 +0600

    add data delete functionlaity in database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app =express();
 const port = process.env.PORT || 5000;
 
@@ -43,6 +43,13 @@ async function run() {
         console.log(newProduct);
         const result = await productCollection.insertOne(newProduct);
         res.send(result)
+    })
+
+    app.delete('/product/:id', async (req, res)=>{
+      const id =req.params.id;
+      const query ={_id: new ObjectId(id)}
+      const result =await productCollection.deleteOne(query);
+      res.send(result);
     })
 
 
